e0766822c653d28281e12a0798343981a0e9c924
Grzegorz Miłoś <gmilos@apple.com>
2016-12-06 00:27:54 +0000

    Repoint to public github.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TestZ",
     dependencies: [
-        .Package(url: "git@github.pie.apple.com:gmilos/CLibZ.git", majorVersion: 0),
+        .Package(url: "git@github.com:gmilos/CLibZ.git", majorVersion: 0),
     ]
 )
